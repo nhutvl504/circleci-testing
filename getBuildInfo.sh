9ea72337a368ceb029407e9803942168ae8586c4
Joseph W Becher <drazisil@users.noreply.github.com>
2015-12-15 03:28:17 -0500

    Stopping for the night
    I'm on https://gist.github.com/drazisil/c36edcb4daf7645654c3#file-build-travis-node-L75

--- getBuildInfo.sh
+++ getBuildInfo.sh
@@ -22,17 +22,17 @@ echo -e "------------------------------"
 node --version
 echo -e "\nPre-installed Redis version"
 echo -e "----------------------------"
-redis --version
+redis-server --version
 echo -e "\nriak version"
 echo -e "------------"
 riak version
 echo -e "\nMongoDB version"
 echo -e "---------------"
-mongodb --version
+mongod --version
 echo -e "\ncouchDB version"
 echo -e "---------------"
-couchdb --version
+curl http://127.0.0.1:5984 | grep -m 1 "version"
 echo -e "\nNeo4j version"
 echo -e "-------------"
-neo4j --version
+curl http://localhost:7474/db/data/ | grep "neo4j_version"
 exit 0
