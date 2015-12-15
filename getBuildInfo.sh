1c8c204f2231ee89ed29764bebeee3a647c67fcc
Joseph W Becher <drazisil@users.noreply.github.com>
2015-12-15 03:14:38 -0500

    Added some more

--- getBuildInfo.sh
+++ getBuildInfo.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 echo -e "Build language: node"
 echo -e ""
-echo -e "Operating System Details"
+echo -e "\nOperating System Details"
 echo -e "------------------------"
 lsb_release -a
-echo -e "Linux Version"
+echo -e "\nLinux Version"
 echo -e "-------------"
 echo -e 
 uname -r
-echo -e "GCC Version"
+echo -e "\nGCC Version"
 echo -e "-------------"
 gcc --version | grep "gcc"
-echo -e "Docker Version"
+echo -e "\nDocker Version"
 echo -e "--------------"
 docker version 2>> /dev/null
+echo -e "\nPre-installed Ruby versions"
+echo -e "---------------------------"
+ruby --version
+echo -e "\nPre-installed Node.js versions"
+echo -e "------------------------------"
+node --version
+echo -e "\nPre-installed Redis versions"
+echo -e "----------------------------"
+redis --version
+echo -e "\nriak version"
+echo -e "------------"
+riak --version
 
 exit 0
