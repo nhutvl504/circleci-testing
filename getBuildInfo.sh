b90c7e42b5b149ef3bd174e2bb71ce21ad272e18
Joseph W Becher <drazisil@users.noreply.github.com>
2015-12-15 02:35:57 -0500

    added some more items

--- getBuildInfo.sh
+++ getBuildInfo.sh
@@ -3,4 +3,15 @@ echo -e "Build language: node"
 echo -e ""
 echo -e "Operating System Details"
 echo -e "------------------------"
-echo -e "Distributor ID: \t $(grep -m 1 "NAME=" $(ls /etc/*-release) | sed 's/PRETTY_NAME=/ /' | sed 's#"# #g')"
+lsb_release -a
+echo -e "Linux Version"
+echo -e "-------------"
+echo -e 
+uname -r
+echo -e "GCC Version"
+echo -e "-------------"
+gcc --version | grep "gcc"
+echo -e "Docker Version"
+echo -e "--------------"
+docket version
+docker version --format '{{.Server.Version}}'
