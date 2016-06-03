8cbb8421f657760abb424b459bab433522916c54
Joseph W Becher <drazisil@users.noreply.github.com>
2016-06-03 08:01:41 -0400

    print is needed

--- check_shared.py
+++ check_shared.py
@@ -1,5 +1,5 @@
 import sysconfig
 print('is Enable Shared?')
-sysconfig.get_config_vars('Py_ENABLE_SHARED')
+print sysconfig.get_config_vars('Py_ENABLE_SHARED')
 print('all python env vars')
-sysconfig.get_config_vars()
+print sysconfig.get_config_vars()
