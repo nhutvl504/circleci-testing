188825912ebc59dc48441f10f8c197397fab2219
Joseph W Becher <drazisil@users.noreply.github.com>
2016-06-07 09:04:36 -0400

    Create circle-test.php
    [ci skip]

---
+++ circle-tests/circle-test.php
@@ -0,0 +1,19 @@
+<?php
+Class CpanelDDNSTest extends PHPUnit_Framework_TestCase
+{
+    // ...
+
+    public function testCanRunTest()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        $this->assertEquals("moo", "moo");
+        $this->assertEquals("moo", "foo");
+        $this->assertEquals("moo", "moo");
+    }
+
+    // ...
+}
