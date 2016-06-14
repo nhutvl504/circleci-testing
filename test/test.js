eb6aa996b600eef76448ddd227314239ee747dda
Joseph W Becher <drazisil@users.noreply.github.com>
2016-06-14 04:53:45 -0400

    Update test.js
    [ci skip]

--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
 var example = require('../example.js');
 var assert = require('chai').assert;
 describe('Example', function() {
-  it('should locate the database file, if sqlite3 is used', function() {
+  it('should all pass', function() {
     assert.equal(example.returnNewText("New text 1"), "New text 1");
   });
   it('2', function() {
-    assert.equal(example.returnNewText("New text 2"), "New text 3");
+    assert.equal(example.returnNewText("New text 2"), "New text 2");
   });
   it('3', function() {
     assert.equal(example.returnNewText("New text 3"), "New text 3");
