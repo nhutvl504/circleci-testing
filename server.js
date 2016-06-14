168ba56bb6f1a8617ab9fc3ee4861a0cfcee10c9
Joseph W Becher <drazisil@users.noreply.github.com>
2016-06-14 05:24:39 -0400

    Made port dynamic

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 8080;
+
 app.use(express.static('static'));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
 });
