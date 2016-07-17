24e27c0d76ae985951c8f59221321df5b7706f09
Joseph W Becher <drazisil@users.noreply.github.com>
2016-07-17 02:19:59 -0400

    Ruby ramblings

---
+++ ruby-file.rb
@@ -0,0 +1,7 @@
+puts "puts reminds me of another languags"
+puts 'was it c?'
+puts "or was it perl?"
+puts 'either way, I'm sure it stands for put string'
+puts 'I wonder what quote stylet I should be using, \" or \''
+puts "..."
+puts 'wasn't I supposed to say "hello world" at so excited point?'
