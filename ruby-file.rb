6f9f3a4720664bf873d882c6b8b513ce57cba817
Joseph W Becher <drazisil@users.noreply.github.com>
2016-07-17 07:33:56 -0500

    Relativity

--- ruby-file.rb
+++ ruby-file.rb
@@ -1,23 +1,4 @@
-# let's get Classical
-class LearnRuby
-  # so, I'm asleep still, so this is even more rambling
-  def some_words()
-    # I should try a gets
-    puts "puts reminds me of another language"
-    # an old man and a young man try to program
-    puts 'was it c?'
-    # whom knows which language? 
-    puts "or was it perl?"
-    # but, put where?
-    puts 'either way, I\'m sure it stands for put string'
-    # a quote by any other name
-    puts 'I wonder what quote style I should be using, \" or \''
-    # I use this too much
-    puts "..."
-    # the universe is lonely, it wants a friend
-    puts 'wasn\'t I supposed to say "hello world" at some point?'
-  end
-end
+require_relative 'learn_ruby'
 
 # I need a class
 ramble = LearnRuby.new
