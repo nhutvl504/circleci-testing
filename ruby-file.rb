82a38a98bb331b5cc65ef2979ea707c3bd2eafcd
Joseph W Becher <drazisil@users.noreply.github.com>
2016-07-17 02:27:11 -0400

    I suppose " would make the auto type happier

--- ruby-file.rb
+++ ruby-file.rb
@@ -4,4 +4,4 @@ puts "or was it perl?"
 puts 'either way, I\'m sure it stands for put string'
 puts 'I wonder what quote style I should be using, \" or \''
 puts "..."
-puts 'wasn't I supposed to say "hello world" at some point?'
+puts 'wasn\'t I supposed to say "hello world" at some point?'
