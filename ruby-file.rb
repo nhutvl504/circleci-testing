31589e0c77f65bdd98ce1287e256f57d391c9950
Joseph W Becher <drazisil@users.noreply.github.com>
2016-07-17 02:54:23 -0400

    Insightful commentary

--- ruby-file.rb
+++ ruby-file.rb
@@ -1,7 +1,14 @@
-puts "puts reminds me of another languags"
+# I should try a gets
+puts "puts reminds me of another language"
+# an old man and a young man try to program
 puts 'was it c?'
+# whom knows which language? 
 puts "or was it perl?"
+# but, put where?
 puts 'either way, I\'m sure it stands for put string'
+# a quote by any other name
 puts 'I wonder what quote style I should be using, \" or \''
+# I use this too much
 puts "..."
+# the universe is lonely, it wants a friend
 puts 'wasn\'t I supposed to say "hello world" at some point?'
