7f3fb1186ecd93802f61ad3acef211397b30d9c8
Joseph W Becher <drazisil@users.noreply.github.com>
2016-07-17 06:48:01 -0500

    The quest for documentation

--- ruby-file.rb
+++ ruby-file.rb
@@ -1,14 +1,20 @@
-# I should try a gets
-puts "puts reminds me of another language"
-# an old man and a young man try to program
-puts 'was it c?'
-# whom knows which language? 
-puts "or was it perl?"
-# but, put where?
-puts 'either way, I\'m sure it stands for put string'
-# a quote by any other name
-puts 'I wonder what quote style I should be using, \" or \''
-# I use this too much
-puts "..."
-# the universe is lonely, it wants a friend
-puts 'wasn\'t I supposed to say "hello world" at some point?'
+# so, I'm asleep still, so this is even more rambling
+def some_words()
+  # I should try a gets
+  puts "puts reminds me of another language"
+  # an old man and a young man try to program
+  puts 'was it c?'
+  # whom knows which language? 
+  puts "or was it perl?"
+  # but, put where?
+  puts 'either way, I\'m sure it stands for put string'
+  # a quote by any other name
+  puts 'I wonder what quote style I should be using, \" or \''
+  # I use this too much
+  puts "..."
+  # the universe is lonely, it wants a friend
+  puts 'wasn\'t I supposed to say "hello world" at some point?'
+end
+
+# can you hear me calling?
+puts some_words
