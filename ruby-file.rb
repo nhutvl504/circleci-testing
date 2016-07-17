2f5974e32ad0b46d9fdd41b3b927d8edc8a547c4
Joseph W Becher <drazisil@users.noreply.github.com>
2016-07-17 07:43:47 -0500

    Two isn't as bad as one

--- ruby-file.rb
+++ ruby-file.rb
@@ -1,7 +1,9 @@
 require_relative 'learn_ruby'
 
 # I need a class
-ramble = LearnRuby.new
+ramble = LearnRuby.new('C')
+second_ramble = LearnRuby.new('Perl')
 
 # can you hear me calling?
 puts ramble.some_words
+puts second_ramble.some_words
