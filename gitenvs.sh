2c8f458e2ef40e77666db929371b4f151336c6c3
Joseph W Becher <drazisil@users.noreply.github.com>
2016-08-30 12:16:15 -0400

    Create file
    [skip ci]

---
+++ gitenvs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -x
+GIT_HASH=$(git rev-parse --short HEAD)
+BUILD_NUMBER=$(rev-list HEAD | wc -l | tr -d ' ')
+GIT_LOG=$(git log --oneline remotes/origin/log..remotes/origin/matrix-node)
