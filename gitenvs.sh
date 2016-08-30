f59cab19e776121e26ac498aace3f639aaccd42d
Joseph W Becher <drazisil@users.noreply.github.com>
2016-08-30 12:31:56 -0400

    Work, darn you.

--- gitenvs.sh
+++ gitenvs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
-GIT_HASH="$(git rev-parse --short HEAD)"
-BUILD_NUMBER="$(git rev-list HEAD | wc -l | tr -d ' ')"
-GIT_LOG="$(git log --oneline remotes/origin/log..remotes/origin/matrix-node)"
+GIT_HASH=$(git rev-parse --short HEAD)"
+BUILD_NUMBER=$(git rev-list HEAD | wc -l | tr -d ' ')
+GIT_LOG=$(git log --oneline remotes/origin/log..remotes/origin/matrix-node)
